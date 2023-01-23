6a79327ecf8251db30957cc41c4d6e524badf1c3
vlejd <vladimir.v.macko@gmail.com>
2023-01-23 10:46:06 +0100

    Globals for skirmish

--- game/dota_skirmish/scripts/vscripts/internal/globals.lua
+++ game/dota_skirmish/scripts/vscripts/internal/globals.lua
@@ -7,5 +7,5 @@ HERO_SELECTION_LENGTH = 600
 START_WITH_PAUSE = true
 CLIENT_PRINT = false
 ALLOW_CUSTOM_SCENARIO = true
-WORKING_BOTS = false		-- BOTS are always added, this just makes them move
-INCLUDED_SCENARIOS = {"n3_ti_rng_entity_87min"}
+WORKING_BOTS = true		-- BOTS are always added, this just makes them move
+INCLUDED_SCENARIOS = {"n2_ti_liquid_ta_1"}
