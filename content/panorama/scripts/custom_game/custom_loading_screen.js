8fca924206e9aece32465e724fa1b476c76dd5ff
EarthSalamander <jeizyy30@hotmail.fr>
2023-10-10 19:33:40 +0200

    fix

--- content/panorama/scripts/custom_game/custom_loading_screen.js
+++ content/panorama/scripts/custom_game/custom_loading_screen.js
@@ -1,19 +1,36 @@
+const delay = 10.0;
+var will_show = false;
+var should_show = true;
+
 (function () {
 	SetSpectatorMessage();
 })();
 
 function SetSpectatorMessage() {
-	const LoadingScreen = $.GetContextPanel().GetParent().GetParent().GetParent();
-
-	if (Game.GetState() == 2 && LoadingScreen && (Game.GetLocalPlayerID() == -1 || Game.GetLocalPlayerID() == null)) {
-	// if (Game.GetState() == 2 && LoadingScreen && (Game.GetLocalPlayerID() == 0 || Game.GetLocalPlayerID() == null)) {
-		// LoadingScreen.visible = false;
-		$("#SpectatorLabel").visible = true;
+	if (Game.GetLocalPlayerID() == -1 && Players.GetTeam(Game.GetLocalPlayerID()) == -1) {
+		if (will_show == false) {
+			$.Msg("Showing spectator message in " + delay + " seconds");
+	
+			will_show = true;
+			$.Schedule(delay, ShowSpectatorMessage);
+		}
 	} else {
 		$("#SpectatorLabel").visible = false;
+
+		if (should_show == true) {
+			$.Msg("Hold on! You are not a spectator!");
+
+			should_show = false;
+		}
 	}
 
 	if (Game.GetState() <= 3) {
 		$.Schedule(1.0, SetSpectatorMessage);
 	}
+}
+
+function ShowSpectatorMessage() {
+	$.Msg("Showing spectator message: " + should_show);
+	$("#SpectatorLabel").visible = should_show;
+	will_show = false;
 }
